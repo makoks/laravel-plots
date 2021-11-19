cbb4f3be4453b27f08c5ab4b07a911589736cc07
makoks <kolesoks@gmail.com>
2021-11-19 10:26:18 +0500

    feat: add plot model

---
+++ app/Domains/Plot/Models/Plot.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Domains\Plot\Models;
+
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+
+class Plot extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'number',
+        'address',
+        'price',
+        'area',
+    ];
+
+    public static function saveBiglandApiPlots($plots) {
+        $result = [];
+        foreach ($plots as $plot) {
+            $savedPlot = self::updateOrCreate(
+                ['number' => Arr::get($plot, 'attrs.plot_number')],
+                [
+                    'address' => Arr::get($plot, 'attrs.plot_address'),
+                    'price' => Arr::get($plot, 'attrs.plot_price'),
+                    'area' => Arr::get($plot, 'attrs.plot_area'),
+                ]
+            );
+            $result[] = $savedPlot;
+            $savedPlot->touch();
+        }
+
+        return collect($result);
+    }
+}
